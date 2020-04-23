fa4e303ff01675bca773684bbb9e034c357053be
Gyasmin <gyasminassuncao@uni9.edu.br>
2020-04-23 15:37:30 -0300

    Terminando primeiro projeto com node e typescript

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,34 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointmentRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
-  const appointment = {
-    provider,
-    date,
-  };
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
 
-  appointments.push(appointment);
-  return response.json(appointment);
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.mesage });
+  }
 });
 
 export default appointmentsRouter;
